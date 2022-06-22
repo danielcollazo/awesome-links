b58305d789efd84bebf281b4cc7f2be94995997d
Daniel Collazo <collazo.daniel@icloud.com>
2022-06-21 22:25:41 -0700

    Remove hard coded list and fetch from the database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import Head from "next/head";
 import { AwesomeLink } from "../components/AwesomeLink";
-import { links } from "../data/links";
+import { gql, useQuery } from "@apollo/client";
+
+const allLinksQuery = gql`
+  query {
+    links {
+      id
+      title
+      url
+      description
+      imageUrl
+      category
+    }
+  }
+`;
 
 export default function Home() {
+  const { loading, error, data } = useQuery(allLinksQuery);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Hmmm.. an error has occurred {error.message} </p>;
+
   return (
     <div>
       <Head>
@@ -12,7 +30,7 @@ export default function Home() {
 
       <div className="container mx-auto max-w-5xl my-20">
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {links.map((link) => (
+          {data?.links.map((link) => (
             <AwesomeLink
               key={link.id}
               url={link.url}
