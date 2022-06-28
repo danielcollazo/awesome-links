ceabf0886edfd7e9f86e40bc789a15f6efb264a7
Daniel Collazo <collazo.daniel@icloud.com>
2022-06-27 19:51:40 -0700

    Implement pagination

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from "nexus";
+import { extendType, intArg, objectType, stringArg } from "nexus";
 import { User } from "./User";
 
 export const Link = objectType({
@@ -25,13 +25,101 @@ export const Link = objectType({
   },
 });
 
+export const Edge = objectType({
+  name: "Edge",
+  definition(t) {
+    t.string("cursor");
+    t.field("node", {
+      type: Link,
+    });
+  },
+});
+
+export const PageInfo = objectType({
+  name: "PageInfo",
+  definition(t) {
+    t.string("endCursor");
+    t.boolean("hasNextPage");
+  },
+});
+
+export const Response = objectType({
+  name: "Response",
+  definition(t) {
+    t.field("pageInfo", { type: PageInfo });
+    t.list.field("edges", { type: Edge });
+  },
+});
+
+// /graphql/types/Link.ts
+// get ALL Links
 export const LinksQuery = extendType({
   type: "Query",
   definition(t) {
-    t.nonNull.list.field("links", {
-      type: Link,
-      resolve(_parent, _args, context) {
-        return context.prisma.link.findMany();
+    t.field("links", {
+      type: "Response",
+      args: {
+        first: intArg(),
+        after: stringArg(),
+      },
+      async resolve(_, args, ctx) {
+        let queryResults = null;
+
+        if (args.after) {
+          // check if there is a cursor as the argument
+          queryResults = await ctx.prisma.link.findMany({
+            take: args.first, // the number of items to return from the database
+            skip: 1, // skip the cursor
+            cursor: {
+              id: args.after, // the cursor
+            },
+          });
+        } else {
+          // if no cursor, this means that this is the first request
+          //  and we will return the first items in the database
+          queryResults = await ctx.prisma.link.findMany({
+            take: args.first,
+          });
+        }
+        // if the initial request returns links
+        if (queryResults.length > 0) {
+          // get last element in previous result set
+          const lastLinkInResults = queryResults[queryResults.length - 1];
+          // cursor we'll return in subsequent requests
+          const myCursor = lastLinkInResults.id;
+
+          // query after the cursor to check if we have nextPage
+          const secondQueryResults = await ctx.prisma.link.findMany({
+            take: args.first,
+            cursor: {
+              id: myCursor,
+            },
+            orderBy: {
+              //   index: "asc",
+            },
+          });
+          // return response
+          const result = {
+            pageInfo: {
+              endCursor: myCursor,
+              hasNextPage: secondQueryResults.length >= args.first, //if the number of items requested is greater than the response of the second query, we have another page
+            },
+            edges: queryResults.map((link) => ({
+              cursor: link.id,
+              node: link,
+            })),
+          };
+
+          return result;
+        }
+        //
+        return {
+          pageInfo: {
+            endCursor: null,
+            hasNextPage: false,
+          },
+          edges: [],
+        };
       },
     });
   },
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,36 @@ import { AwesomeLink } from "../components/AwesomeLink";
 import { gql, useQuery } from "@apollo/client";
 
 const allLinksQuery = gql`
-  query {
-    links {
-      id
-      title
-      url
-      description
-      imageUrl
-      category
+  query allLinksQuery($first: Int, $after: String) {
+    links(first: $first, after: $after) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          id
+          title
+          url
+          description
+          imageUrl
+          category
+        }
+      }
     }
   }
 `;
 
 export default function Home() {
-  const { loading, error, data } = useQuery(allLinksQuery);
+  const { loading, error, data, fetchMore } = useQuery(allLinksQuery, {
+    variables: { first: 2 },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Hmmm.. an error has occurred {error.message} </p>;
+
+  const { endCursor, hasNextPage } = data.links.pageInfo;
 
   return (
     <div>
@@ -29,19 +42,42 @@ export default function Home() {
       </Head>
 
       <div className="container mx-auto max-w-5xl my-20">
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {data?.links.map((link) => (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {data?.links.edges.map(({ node }) => (
             <AwesomeLink
-              key={link.id}
-              url={link.url}
-              id={link.id}
-              category={link.category}
-              title={link.title}
-              description={link.description}
-              imageUrl={link.imageUrl}
+              key={node.id}
+              url={node.url}
+              id={node.id}
+              category={node.category}
+              title={node.title}
+              description={node.description}
+              imageUrl={node.imageUrl}
             />
           ))}
-        </ul>
+        </div>
+        {hasNextPage ? (
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded my-10"
+            onClick={() =>
+              fetchMore({
+                variables: { after: endCursor },
+                updateQuery: (prevResult, { fetchMoreResult }) => {
+                  fetchMoreResult.links.edges = [
+                    ...prevResult.links.edges,
+                    ...fetchMoreResult.links.edges,
+                  ];
+                  return fetchMoreResult;
+                },
+              })
+            }
+          >
+            more
+          </button>
+        ) : (
+          <p className="my-10 text-center font-medium">
+            You have reached the end!
+          </p>
+        )}
       </div>
     </div>
   );
