a58ecdf17c334a99cbcb14dd6e9a479d5516d5ee
Daniel Collazo <collazo.daniel@icloud.com>
2022-07-01 23:44:56 -0700

    Configure auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import "../styles/tailwind.css";
+import { UserProvider } from "@auth0/nextjs-auth0";
 import Layout from "../components/Layout";
 import { ApolloProvider } from "@apollo/client";
 import apolloClient from "../lib/apollo";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </UserProvider>
   );
 }
 
