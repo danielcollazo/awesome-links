3b78d4f67cb1bcb8f1791797083a25f2d52b1fd3
Daniel Collazo <collazo.daniel@icloud.com>
2022-07-01 23:45:26 -0700

    Secure the graphql api

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,12 +1,24 @@
+import { Claims, getSession } from "@auth0/nextjs-auth0";
 import { PrismaClient } from "@prisma/client";
 import prisma from "../lib/prisma";
 
 export type Context = {
+  user?: Claims;
+  accessToken?: string;
   prisma: PrismaClient;
 };
 
-export async function createContext(req, res): Promise<Context> {
+export async function createContext({ req, res }): Promise<Context> {
+  const session = getSession(req, res);
+
+  // if the user is not logged in, omit returning the user and accessToken
+  if (!session) return { prisma };
+
+  const { user, accessToken } = session;
+
   return {
+    user,
+    accessToken,
     prisma,
   };
 }
