bd94e04c764b62f47db065a8e66f126e6c0b9c05
Daniel Collazo <collazo.daniel@icloud.com>
2022-07-14 17:28:31 -0700

    Remove hard coded apollo uri

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: process.env.NEXT_PUBLIC_APOLLO_ENDPOINT,
   cache: new InMemoryCache(),
 });
 
